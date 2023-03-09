37bdd18f832a3ad1825ca1ddfb0c1e2cdb1e07e3
cold0sky <1433132615@qq.com>
2023-03-09 14:19:07 +0800

    20230308

---
+++ src/main/java/com/coldsky/vhr/entity/HrRole.java
@@ -0,0 +1,34 @@
+package com.coldsky.vhr.entity;
+
+// 用户角色类
+public class HrRole {
+    private Integer id;
+
+    private Integer hrid;
+
+    private Integer rid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getHrid() {
+        return hrid;
+    }
+
+    public void setHrid(Integer hrid) {
+        this.hrid = hrid;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+}
---
+++ src/main/java/com/coldsky/vhr/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.coldsky.vhr.service;
+
+import com.coldsky.vhr.entity.Menu;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuService {
+    public List<Menu> getAllMenu() {
+        return null;
+    }
+}
