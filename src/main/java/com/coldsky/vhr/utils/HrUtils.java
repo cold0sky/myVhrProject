fb0b1a1d79860086ac693a6934175d9a959f123f
cold0sky <1433132615@qq.com>
2023-03-21 17:49:00 +0800

    项目文档

--- src/main/java/com/coldsky/vhr/utils/HrUtils.java
+++ src/main/java/com/coldsky/vhr/utils/HrUtils.java
@@ -3,8 +3,12 @@ package com.coldsky.vhr.utils;
 import com.coldsky.vhr.model.Hr;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class HrUtils {
+    ConcurrentHashMap
     // 获取当前用户
     public static Hr getCurrentHr() {
         return ((Hr) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
